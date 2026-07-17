f6f78a9839a427a76c842f77f675af8a4c409317
Raphael Henrique Caldeira dos Santos <65393837+neorh@users.noreply.github.com>
2026-07-17 16:11:20 -0300

    Refactor imports and improve bot initialization

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-import langchain
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
